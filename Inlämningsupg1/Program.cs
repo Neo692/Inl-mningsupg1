0c20293f6e5077434e967d9600442ce9eb9e1131
nehy06001 <nehy06001@E-PW05CGQV>
2024-10-03 13:56:21 +0200

    Add project files.

---
+++ Inlämningsupg1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace inlämningsupg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Din favoritbok är " + bok);
+        }
+    }
+}
